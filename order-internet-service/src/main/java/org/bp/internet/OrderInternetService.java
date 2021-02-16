b9b381ae6c48293aa7076defb36a26104ea928e5
Wojciech Przybylski <wojciech.s.przybylski@student.put.poznan.pl>
2021-02-17 00:26:30 +0100

    Implement canceling and getting order

--- order-internet-service/src/main/java/org/bp/internet/OrderInternetService.java
+++ order-internet-service/src/main/java/org/bp/internet/OrderInternetService.java
@@ -11,23 +11,49 @@ import org.bp.types.InternetOrder;
 @javax.jws.WebService
 @org.springframework.stereotype.Service
 public class OrderInternetService {
-	
+
 	HashMap<Integer, OrderInfo> orders = new HashMap<>();
-	
-	public OrderInfo orderInternet(InternetOrder internetOrder) throws Fault{
+
+	public OrderInfo orderInternet(InternetOrder internetOrder) throws Fault {
 		ExceptionHandler.validateRequest(internetOrder);
 
 		OrderInfo internetOrderInfo = new OrderInfo();
 		internetOrderInfo.setId(getOrderId(orders));
 		internetOrderInfo.setCost(internetOrder.getInternetService().calcCost());
-		
-		return internetOrderInfo;	
+		orders.put(internetOrderInfo.getId(), internetOrderInfo);
+
+		return internetOrderInfo;
 	}
-	
+
+	public OrderInfo getOrderInfo(OrderInfo orderInfo) throws Fault {
+		OrderInfo orderInfoResponse = orders.get(orderInfo.getId());
+
+		if (orderInfoResponse == null) {
+			Fault fault = new Fault();
+			fault.setCode(50);
+			fault.setText("There is no order with id " + orderInfo.getId());
+			throw fault;
+		}
+
+		return orderInfoResponse;
+	}
+
 	public OrderInfo cancelOrder(int orderId) throws Fault {
-		return null;
+		OrderInfo orderInfoResponse;
+
+		if (orders.containsKey(orderId)) {
+			orderInfoResponse = orders.get(orderId);
+			orders.remove(orderId);
+		} else {
+			Fault fault = new Fault();
+			fault.setCode(60);
+			fault.setText("There is no order with id " + orderId);
+			throw fault;
+		}
+
+		return orderInfoResponse;
 	}
-	
+
 	private int getOrderId(HashMap<Integer, OrderInfo> orders) {
 		Random rand = new Random();
 		int id = 1;
